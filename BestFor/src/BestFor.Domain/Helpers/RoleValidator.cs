48a322816a089128b941fc894d6f6204c95dba39
anatolitrifonov <atrifonov@atrifonov.com>
2016-03-15 21:44:14 -0700

    One file was missing

---
+++ BestFor/src/BestFor.Domain/Helpers/RoleValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using BestFor.Domain.Entities;
+
+namespace BestFor.Domain.Helpers
+{
+    public class RoleValidator : IRoleValidator<IdentityRole>
+    {
+        /// <summary>
+        /// I have no idea what this interface should do. Comments suck.
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public Task<IdentityResult> ValidateAsync(RoleManager<IdentityRole> manager, IdentityRole role)
+        {
+            IdentityResult result = new IdentityResult();
+            return Task.FromResult<IdentityResult>(result);
+        }
+    }
+}
