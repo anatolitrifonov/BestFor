ef1e65b023afb4b68cdc9bdd9ce2ee069e456fd9
Anatoli Trifonov <atrifonov@atrifonov.com>
2016-10-01 13:20:11 -0700

    Adding tests for controllers. And test settings. Will save before makin ghome page changes.

---
+++ BestFor/BestFor.UnitTests/Testables/TestLoggerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace BestFor.UnitTests.Testables
+{
+    [ExcludeFromCodeCoverage]
+    public class TestLoggerFactory: ILoggerFactory
+    {
+        public void AddProvider(ILoggerProvider provider)
+        {
+
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new TestLogger<object>();
+        }
+
+        public void Dispose()
+        {
+            // intentionally does nothing
+        }
+    }
+}
